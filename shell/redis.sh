e9231168b97472ea94596572184406ae9dd8194d
João Pereira <jaca.pereira@campus.fct.unl.pt>
2022-05-23 22:03:42 +0100

    Added persistency to redis

--- shell/redis.sh
+++ shell/redis.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --network net --name "redis" --ip "172.18.0.21" -d redis 
+docker run --network net --name "redis" --ip "172.18.0.21" -d redis --save 30 1 --loglevel warning
