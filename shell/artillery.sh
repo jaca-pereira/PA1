f672a9966cd628eb147a7c3ff7b5e4fba03c370f
João Pereira <jaca.pereira@campus.fct.unl.pt>
2022-06-30 20:44:54 +0100

    changed some artillery stuff

--- shell/artillery.sh
+++ shell/artillery.sh
@@ -10,17 +10,14 @@ docker run --network net --ip "172.19.0.43" --name "start_3" -p 8080 -d artiller
 
 sleep 7
 
-docker run --network net --ip "172.19.0.40" --name "worload0" -p 8080 artillery run --output report_workload_0.json workload_0.yml
-docker run --network net --ip "172.19.0.41" --name "worload1" -p 8080 -d artillery run --output report_workload_1.json workload_1.yml
-docker run --network net --ip "172.19.0.42" --name "worload2" -p 8080 -d artillery run --output report_workload_2.json workload_2.yml
-docker run --network net --ip "172.19.0.43" --name "worload3" -p 8080 -d artillery run --output report_workload_3.json workload_3.yml
+docker run --network net --ip "172.19.0.40" --name "worload_0" -p 8080 artillery run --output report_workload_0.json workload_0.yml
+docker run --network net --ip "172.19.0.41" --name "worload_1" -p 8080 -d artillery run --output report_workload_1.json workload_1.yml
+docker run --network net --ip "172.19.0.42" --name "worload_2" -p 8080 -d artillery run --output report_workload_2.json workload_2.yml
+docker run --network net --ip "172.19.0.43" --name "worload_3" -p 8080 -d artillery run --output report_workload_3.json workload_3.yml
 
 sleep 17
 
-docker run --network net --ip "172.19.0.40" --name "mine_0" -p 8080 -d artillery run --output report_mine_1.json mine_1.yml
-#docker run --network net --ip "172.19.0.41" --name "mine_1" -p 8080 -d artillery run --output report_mine_2.json mine_2.yml
-#docker run --network net --ip "172.19.0.42" --name "mine_2" -p 8080 -d artillery run --output report_mine_3.json mine_3.yml
-
+docker run --network net --ip "172.19.0.40" --name "mine_0" -p 8080 -d artillery run --output report_mine_0.json mine_0.yml
 
 #artillery report report_start_0.json
 #artillery report report_start_1.json
