c8e60e9b54c77fab108e7c09853e1fdc6b15ae21
João Pereira <62773204+jaca-pereira@users.noreply.github.com>
2022-07-22 16:35:49 +0100

    Update artillery.sh

--- shell/artillery.sh
+++ shell/artillery.sh
@@ -2,21 +2,15 @@
 
 cd ../artillery
 
-if [ $# -eq 0 ] ; then
-    echo "Usage: sh artillery.sh <id>"
-    exit 1
-fi
 
-ID=$1
+artillery run --output report_start_0.json start_0.yml
 
-artillery run --output report_start_$ID.json start_$ID.yml
-
-artillery run --output report_workload_$ID.json workload_$ID.yml
+artillery run --output report_workload_0.json workload_0.yml
 
 
-artillery report report_start_$ID.json
+artillery report report_start_0.json
 
-artillery report report_workload_$ID.json
+artillery report report_workload_0.json
 
 
 
